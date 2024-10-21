280b4c546c765cb1ddc9cb6f6e0d21fe862707e5
Miguel Lemos <miguel_lemos@apple.com>
2024-10-21 04:56:21 +0200

    It works

--- examples/advanced/main.rs
+++ examples/advanced/main.rs
@@ -5,15 +5,15 @@ use bevy::{
     pbr::CascadeShadowConfigBuilder,
     window::CursorGrabMode,
 };
-use bevy_fn_plugin::bevy_plugin;
 use bevy_voxel_world::prelude::*;
+use serde::Deserialize;
 
 mod camera;
 mod voxel;
 use camera::*;
 use voxel::get_voxel_fn;
 
-#[derive(Resource, Clone, Copy)]
+#[derive(Resource, Clone, Deserialize, Default)]
 struct VoxelWorldConfiguration {
     scale: f32,
     height_scale: f32,
@@ -22,129 +22,85 @@ struct VoxelWorldConfiguration {
     to: u32,
 }
 
-const HIGH_DETAIL_CONFIG: VoxelWorldConfiguration = VoxelWorldConfiguration {
-    scale: 1.0,
-    height_scale: 1.0,
-    height_minus: 1.0,
-    from: 0,
-    to: 3,
-};
+macro_rules! impl_voxel_world_config {
+    ($type:ident) => {
+        #[derive(Resource, Clone, Default)]
+        struct $type(VoxelWorldConfiguration);
 
-const LOW_DETAIL_CONFIGS: [VoxelWorldConfiguration; 4] = [
-    VoxelWorldConfiguration {
-        scale: 2.0,
-        height_scale: 0.5,
-        height_minus: 1.0,
-        from: 4,
-        to: 6,
-    },
-    VoxelWorldConfiguration {
-        scale: 4.0,
-        height_scale: 1.0,
-        height_minus: 1.0,
-        from: 7,
-        to: 10,
-    },
-    VoxelWorldConfiguration {
-        scale: 8.0,
-        height_scale: 1.0,
-        height_minus: 1.0,
-        from: 11,
-        to: 15,
-    },
-    VoxelWorldConfiguration {
-        scale: 16.0,
-        height_scale: 1.0,
-        height_minus: 1.0,
-        from: 15,
-        to: 20,
-    },
-];
-
-impl Default for VoxelWorldConfiguration {
-    fn default() -> Self {
-        Self {
-            scale: 1.0,
-            height_scale: 1.0,
-            height_minus: 0.0,
-            from: 6,
-            to: 7,
+        impl VoxelWorldConfig for $type {
+            fn spawning_min_distance(&self) -> u32 {
+                self.0.from
+            }
+            fn spawning_distance(&self) -> u32 {
+                self.0.from
+            }
+            fn spawning_max_distance(&self) -> u32 {
+                self.0.to
+            }
+            fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate {
+                let scale = self.0.scale as f64;
+                let height_scale = self.0.height_scale as f64;
+                let height_minus = self.0.height_minus as f64;
+                Box::new(move |_chunk_pos| {
+                    let mut voxel_fn = get_voxel_fn(scale, height_scale, height_minus);
+                    Box::new(move |pos| voxel_fn(pos, 0))
+                })
+            }
+            fn chunk_despawn_strategy(&self) -> ChunkDespawnStrategy {
+                ChunkDespawnStrategy::Distance(7)
+            }
+            fn chunk_spawn_strategy(&self) -> ChunkSpawnStrategy {
+                ChunkSpawnStrategy::Distance(5)
+            }
+            fn debug_draw_chunks(&self) -> bool {
+                false
+            }
         }
-    }
+    };
 }
 
-impl VoxelWorldConfig for VoxelWorldConfiguration {
-    fn spawning_min_distance(&self) -> u32 {
-        self.from
-    }
-    fn spawning_distance(&self) -> u32 {
-        self.from
-    }
-    fn spawning_max_distance(&self) -> u32 {
-        self.to
-    }
-    fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate {
-        let scale = self.scale as f64;
-        let height_scale = self.height_scale as f64;
-        let height_minus = self.height_minus as f64;
-        Box::new(move |_chunk_pos| {
-            let mut voxel_fn = get_voxel_fn(scale, height_scale, height_minus);
-            Box::new(move |pos| voxel_fn(pos, 0))
-        })
-    }
-    fn chunk_despawn_strategy(&self) -> ChunkDespawnStrategy {
-        ChunkDespawnStrategy::Distance(7)
-    }
-    fn chunk_spawn_strategy(&self) -> ChunkSpawnStrategy {
-        ChunkSpawnStrategy::Distance(5)
-    }
-    fn debug_draw_chunks(&self) -> bool {
-        false
-    }
+impl_voxel_world_config!(HighDetailWorld);
+impl_voxel_world_config!(LowDetailWorld1);
+impl_voxel_world_config!(LowDetailWorld2);
+impl_voxel_world_config!(LowDetailWorld3);
+impl_voxel_world_config!(LowDetailWorld4);
+
+#[derive(Deserialize)]
+struct WorldConfigurations {
+    high_detail: VoxelWorldConfiguration,
+    low_detail: Vec<VoxelWorldConfiguration>,
 }
 
-fn create_world_plugin(_name: &str, config: VoxelWorldConfiguration) -> impl Plugin {
-    VoxelWorldPlugin::with_config(config)
-}
-
-// High detail world plugin
-#[bevy_plugin]
-fn HighDetailWorldPlugin(app: &mut App) {
-    app.add_plugins(VoxelWorldPlugin::<VoxelWorldConfiguration>::with_config(
-        HIGH_DETAIL_CONFIG,
-    ));
-}
-
-// Low detail world plugins
-#[bevy_plugin]
-fn LowDetailWorld1Plugin(app: &mut App) {
-    app.add_plugins(create_world_plugin("low_detail_1", LOW_DETAIL_CONFIGS[0]));
-}
-
-#[bevy_plugin]
-fn LowDetailWorld2Plugin(app: &mut App) {
-    app.add_plugins(create_world_plugin("low_detail_2", LOW_DETAIL_CONFIGS[1]));
-}
-
-#[bevy_plugin]
-fn LowDetailWorld3Plugin(app: &mut App) {
-    app.add_plugins(create_world_plugin("low_detail_3", LOW_DETAIL_CONFIGS[2]));
-}
-
-#[bevy_plugin]
-fn LowDetailWorld4Plugin(app: &mut App) {
-    app.add_plugins(create_world_plugin("low_detail_4", LOW_DETAIL_CONFIGS[3]));
+fn load_configurations() -> WorldConfigurations {
+    let config_str =
+        std::fs::read_to_string("voxel_configs.yaml").expect("Failed to read config file");
+    serde_yaml::from_str(&config_str).expect("Failed to parse YAML")
 }
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(HighDetailWorldPlugin)
-        .add_plugins(LowDetailWorld1Plugin)
-        .add_plugins(LowDetailWorld2Plugin)
-        .add_plugins(LowDetailWorld3Plugin)
-        .add_plugins(LowDetailWorld4Plugin)
-        .add_systems(Startup, (setup, grab_mouse))
+    let configs = load_configurations();
+
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+
+    app.add_plugins(VoxelWorldPlugin::<HighDetailWorld>::with_config(
+        HighDetailWorld(configs.high_detail),
+    ));
+
+    app.add_plugins(VoxelWorldPlugin::<LowDetailWorld1>::with_config(
+        LowDetailWorld1(configs.low_detail[0].clone()),
+    ));
+    app.add_plugins(VoxelWorldPlugin::<LowDetailWorld2>::with_config(
+        LowDetailWorld2(configs.low_detail[1].clone()),
+    ));
+    app.add_plugins(VoxelWorldPlugin::<LowDetailWorld3>::with_config(
+        LowDetailWorld3(configs.low_detail[2].clone()),
+    ));
+    app.add_plugins(VoxelWorldPlugin::<LowDetailWorld4>::with_config(
+        LowDetailWorld4(configs.low_detail[3].clone()),
+    ));
+
+    app.add_systems(Startup, (setup, grab_mouse))
         .add_systems(Update, (fly_camera, exit_on_esc))
         .insert_resource(ClearColor(Color::srgb(0.5, 0.8, 1.0)))
         .run();
@@ -163,7 +119,11 @@ fn setup(mut commands: Commands) {
 
     commands
         .entity(camera_entity)
-        .insert(VoxelWorldCamera::<VoxelWorldConfiguration>::default());
+        .insert(VoxelWorldCamera::<HighDetailWorld>::default())
+        .insert(VoxelWorldCamera::<LowDetailWorld1>::default())
+        .insert(VoxelWorldCamera::<LowDetailWorld2>::default())
+        .insert(VoxelWorldCamera::<LowDetailWorld3>::default())
+        .insert(VoxelWorldCamera::<LowDetailWorld4>::default());
 
     let cascade_shadow_config = CascadeShadowConfigBuilder::default().build();
     commands.spawn(DirectionalLightBundle {
